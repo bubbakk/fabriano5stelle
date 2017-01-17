0d98f809ce649ad107742ea68438693bfd66be95
andrea.ferroni@spesonline.com <andrea.ferroni@spesonline.com>
2017-01-17 11:22:07 +0100

    Inizio nuova implementazione

---
+++ src/candidati2017.js
@@ -0,0 +1,15 @@
+var CANDIDATI_2017 = [
+    {
+        name: "Andrea Ferroni",
+        img: "bubba.jpg",
+        age: 40,
+        job: "progettista e sviluppatore software",
+        story: "Ho decido di candidarmi perché ...."
+    },
+    {
+        name: "Guido La Rovere",
+        age: 39,
+        job: "imprenditore multimediale ad alto tasso di innovazione",
+        story: "Ho decido di candidarmi perché ...."
+    }
+];
